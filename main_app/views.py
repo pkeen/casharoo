c6e7518b0de19344f4524d6919631543a80ce6f4
Michael Rose <michael@rosenetwork.net>
2023-08-16 11:41:08 -0700

    class based views for Account and Transaction

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,51 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from .models import Account, Transaction
 
 
 def home(request):
     return render(request, "home.html")
+
+
+class AccountCreate(CreateView):
+    model = Account
+    fields = "__all__"
+
+
+class AccountUpdate(UpdateView):
+    model = Account
+    fields = "__all__"
+
+    class AccountDelete(DeleteView):
+        model = Account
+        success_url = "/"
+
+
+class AccountDetail(DetailView):
+    model = Account
+
+    class AccountList(ListView):
+        model = Account
+        fields = "__all__"
+
+class TransactionCreate(CreateView):
+    model = Transaction
+    fields = "__all__"
+
+
+class TransactionUpdate(UpdateView):
+    model = Transaction
+    fields = "__all__"
+
+    class TransactionDelete(DeleteView):
+        model = Transaction
+        success_url = "/"
+
+
+class TransactionDetail(DetailView):
+    model = Transaction
+
+    class TransactionList(ListView):
+        model = Transaction
+        fields = "__all__"
