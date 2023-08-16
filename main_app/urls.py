a80e9cec9fdfa700b8e3c11d709ffb17abe35ad3
Michael Rose <michael@rosenetwork.net>
2023-08-16 15:33:45 -0700

    basic routeds needed

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,16 @@
 from django.urls import path, include
 from . import views
 
-urlpatterns = [path("", views.home, name="home")]
+urlpatterns = [
+    path("", views.home, name="home"),
+    #  accounts
+    path("accounts/", views.AccountList.as_view(), name="index"),
+    path("accounts/create/", views.AccountCreate.as_view(), name="account_create"),
+    path(
+        "accounts/<int:pk>/update", views.AccountUpdate.as_view(), name="account_update"
+    ),
+    path(
+        "accounts/<int:pk>/delete", views.AccountDelete.as_view(), name="account_delete"
+    ),
+    path("accounts/<int:pk>/", views.AccountDetail.as_view(), name="account_detail"),
+]
