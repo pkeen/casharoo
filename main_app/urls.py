97db0ea96ed8c317f36e7b1c8707b490cc0b6c03
Rjaysesh0207 <rnishimoto96@gmail.com>
2023-08-16 16:11:38 -0700

    added an accounts list view and page

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
 from . import views
 
-urlpatterns = [path("", views.home, name="home")]
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("accounts/", views.AccountList.as_view(), name="accounts_list"),
+    ]
