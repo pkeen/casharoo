c1260e48f9eba164d7826d60eeebdf0c5a041dbb
Rjaysesh0207 <rnishimoto96@gmail.com>
2023-08-16 09:28:03 -0700

    corrected name for database

--- casharoo/settings.py
+++ casharoo/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "casharoo.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgres",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "casharoo",
     }
 }
